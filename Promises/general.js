79692d4cebbd7529f356bccf8525070f0ac2e8f7
Ruslan <ruslanbegun@gmail.com>
2019-07-20 19:10:39 +0300

    Promises

---
+++ Promises/general.js
@@ -0,0 +1,26 @@
+let promise = new Promise((resolve, reject) => {
+  let startTime = new Date().getTime();
+  setTimeout(() => {
+    let finishTime = new Date().getTime();
+    let doneTime = (finishTime - startTime) / 1000;
+    Math.random() > 0.5 ? resolve(`TRUE!!! Time: ${doneTime}`) : reject(`FALSE!!! Time: ${doneTime}`);
+  }, Math.random() * 10000);
+});
+
+promise.then(
+  (data) => {console.log(data)},
+  (error) => {console.log(error)}
+  );
+
+/*let counter = 0;
+console.log(counter);
+let intervalId = setInterval(() => {
+  counter++;
+  console.log(counter);
+  if(counter >= 10) clearInterval(intervalId);
+}, 1000);*/
+
+
+/*setInterval(() => {
+  // console.clear();
+}, 1000);*/
